ada2ca98ab7254d1165edc2f3c1c28dbfb0efb65
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-01-29 19:18:24 -0600

    cleanup and colors

--- app.py
+++ app.py
@@ -27,30 +27,12 @@ from io import BytesIO
 
 
 app = Flask(__name__)
-photos = UploadSet("photos", IMAGES)
 
-app.config["UPLOADS_DEFAULT_DEST"] = "static/img"
-app.config["UPLOADS_DEFAULT_URL"] = "/static/img"
-
-configure_uploads(app, photos)
-
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
-
-
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+if __name__ == "__main__":
+    app.run()
